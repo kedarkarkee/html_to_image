4b01a6b707ac1ceda25693b0ccb7fd896a14bb5a
Kedar Karki <karki53327@gmail.com>
2025-05-30 11:50:37 +0545

    feat: add symmetric and all margins to ImageMargins

--- lib/config.dart
+++ lib/config.dart
@@ -11,4 +11,20 @@ class ImageMargins {
     this.bottom = 0,
     this.left = 0,
   }) : assert(top >= 0 && right >= 0 && bottom >= 0 && left >= 0);
+
+  const ImageMargins.symmetric({
+    int horizontal = 0,
+    int vertical = 0,
+  })  : assert(horizontal >= 0 && vertical >= 0),
+        left = horizontal,
+        right = horizontal,
+        top = vertical,
+        bottom = vertical;
+
+  const ImageMargins.all(int value)
+      : assert(value >= 0),
+        left = value,
+        right = value,
+        top = value,
+        bottom = value;
 }
